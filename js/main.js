9e4c8932449a5627da9df64e17328fe3101b3575
Alexlg <Alejandrolg86@gmail.com>
2022-05-27 16:47:16 +0200

    Rehaciendo divs e imagenes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
-let luchador1;
-let luchador2;
-let luchador3;
-let luchador4;
 
 const cambiarPantalla = (screenDestino) => {
 
     let destino = document.getElementById(screenDestino);
     destino.style.display = "flex";
 
-    let arrayDestino = ["pantallaPrincipal","pantallaSeleccionpj"];
+    let arrayDestino = ["pantallaPrincipal","pantallaSeleccionPj"];
 
     for (let screen of arrayDestino){
         if (screen != screenDestino){
@@ -17,8 +13,36 @@ const cambiarPantalla = (screenDestino) => {
     };
 }
 
+let luchador1 = "";
+let luchador2 = "";
+let luchador3 = "";
+let luchador4 = "";
+
+let turnoLuchador = 1;
 
 
-const seleccionarPersonaje = () => {
+const seleccionarLuchador = (seleccionado) => {
+    
+    switch (turnoLuchador){
+        case 1:
+            luchador1 = conjuntoLuchadores[seleccionado];
+            break;
+        case 2:
+            luchador2 = conjuntoLuchadores[seleccionado];
+            break;
+        case 3:
+            luchador3 = conjuntoLuchadores[seleccionado];
+            break;
+        case 4:
+            luchador4 = conjuntoLuchadores[seleccionado];
+            break;     
+        default:
+            break;     
+    };
 
+    turnoLuchador++;
+
+    if (turnoLuchador == 2){
+        console.log("luchadores seleccionados");
+    };
 }
