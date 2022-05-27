3332886f001233ab21629c5a90782e6d64224b02
Alexlg <Alejandrolg86@gmail.com>
2022-05-27 09:20:23 +0200

    añadiendo diccionario

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
+let luchador1;
+let luchador2;
+let luchador3;
+let luchador4;
 
-const cambiarPantalla = (pantallaDestino) => {
+const cambiarPantalla = (screenDestino) => {
 
-    let cambiar = document.getElementById(pantallaDestino)
+    let destino = document.getElementById(screenDestino);
+    destino.style.display = "flex";
+
+    let arrayDestino = ["pantallaPrincipal","pantallaSeleccionpj"];
+
+    for (let screen of arrayDestino){
+        if (screen != screenDestino){
+            document.getElementById(screen).style.display = "none";
+        };
+    };
+}
 
 
 
-
-
+const seleccionarPersonaje = () => {
 
 }
