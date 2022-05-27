ca6b5499186294811e5145381174ce8be05e7a38
Alexlg <Alejandrolg86@gmail.com>
2022-05-27 19:53:25 +0200

    añadiendo opacidad

--- js/main.js
+++ js/main.js
@@ -1,48 +1,28 @@
+let luchadores = [];
 
 const cambiarPantalla = (screenDestino) => {
 
-    let destino = document.getElementById(screenDestino);
-    destino.style.display = "flex";
+    setTimeout(()=>{
+        let destino = document.getElementById(screenDestino);
+        destino.style.display = "flex";
 
-    let arrayDestino = ["pantallaPrincipal","pantallaSeleccionPj"];
+        let arrayDestino = ["pantallaPrincipal","pantallaSeleccionPj"];
 
-    for (let screen of arrayDestino){
-        if (screen != screenDestino){
-            document.getElementById(screen).style.display = "none";
+        for (let screen of arrayDestino){
+            if (screen != screenDestino){
+                document.getElementById(screen).style.display = "none";
+            };
         };
-    };
+    }, 2000);
 }
 
-let luchador1 = "";
-let luchador2 = "";
-let luchador3 = "";
-let luchador4 = "";
+const seleccionarLuchador = (luchador) => {
 
-let turnoLuchador = 1;
+    if (luchadores.length < 2){
 
-
-const seleccionarLuchador = (seleccionado) => {
-    
-    switch (turnoLuchador){
-        case 1:
-            luchador1 = conjuntoLuchadores[seleccionado];
-            break;
-        case 2:
-            luchador2 = conjuntoLuchadores[seleccionado];
-            break;
-        case 3:
-            luchador3 = conjuntoLuchadores[seleccionado];
-            break;
-        case 4:
-            luchador4 = conjuntoLuchadores[seleccionado];
-            break;     
-        default:
-            break;     
-    };
-
-    turnoLuchador++;
-
-    if (turnoLuchador == 2){
-        console.log("luchadores seleccionados");
-    };
+        luchadores.push(conjuntoLuchadores[luchador]);
+        console.log(luchadores);
+        let opaco = document.getElementById(luchador);
+        opaco.style.opacity = 0.4;
+    }
 }
